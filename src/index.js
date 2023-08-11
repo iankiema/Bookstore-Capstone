fbe5af7656cfc657ef3975a1c0530429738741e2
Ian Kiema <112932127+iankiema@users.noreply.github.com>
2023-08-12 01:25:01 +0300

    Added the main files and testing files

--- src/index.js
+++ src/index.js
@@ -1,39 +1,9 @@
+import './main.css';
+import './pop.css';
+import './myreset.css';
 import './style.css';
+import FetchBooks from './modules/bookfetchfunction.js'; // Update the module import
+import BookDetails from './modules/getbook.js'; // Update the module import
 
-const serachQuery = ' romance ';
-const url = `https://openlibrary.org/search.json?q=${encodeURIComponent(serachQuery)}`;
-async function fetchBooks() {
-  const response = await fetch(url);
-  const data = await response.json();
-  return data.docs.map((book) => ({
-    title: book.title,
-    author: book.author ? book.author_name.join(',') : 'Unknown Author',
-    coverUrl: `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`,
-  }));
-}
-const BooksEl = document.querySelector('.Books');
-function dispalyBooks(Books) {
-  Books.forEach((book) => {
-    BooksEl.innerHTML += `
-            <div id="" class="book-card">
-            <div class="book-img">
-            <img src="${book.coverUrl}" alt="${book.title}" />
-            </div>
-            <div id="" class="book-content">
-            <h4 class="book-name">${book.title}</h4>
-            <div id="" class="book-likes">
-                <i class="bi bi-heart"></i>
-                <span id="" class="n-likes"> likes</span>
-            </div>
-            </div>
-            <button id="" class="comment-button">Comment</button>
-        </div>
-  `;
-  });
-}
-
-async function fetchAndDisplayBooks() {
-  const books = await fetchBooks();
-  dispalyBooks(books);
-}
-fetchAndDisplayBooks();
+FetchBooks.fetchBooks();
+BookDetails.commentFn();
